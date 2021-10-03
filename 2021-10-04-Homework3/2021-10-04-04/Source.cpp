b0d4571580a1a246260f0406c04f346885628f01
st080579 <st080579@student.spbu.ru>
2021-10-03 23:30:47 +0300

    2021-10-03

--- 2021-10-04-Homework3/2021-10-04-04/Source.cpp
+++ 2021-10-04-Homework3/2021-10-04-04/Source.cpp
@@ -1,4 +1,5 @@
-#include<iostream>
+#include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -6,33 +7,24 @@ int main(int argc, char* argv[])
 {
 	int n = 0;
 	int k = 0;
-	int factn = 0;
-	int factk = 0;
-	int factnk = 0;
+	double c = 0;
+	int C = 0;
 
 	cout << "Enter data:" << endl;
 	cin >> n >> k;
 	cout << "Exit data:" << endl;
 
-	factn = 1;
-	for (int i = 1; i <= n; ++i)
+	c = 1;
+	C = 1;
+
+	for (int i = 1; i <= n - k; i++)
 	{
-		factn = factn * i;
+		double j = i;
+		c *= (k + j) / j;
 	}
 
-	factk = 1;
-	for (int i = 1; i <= k; ++i)
-	{
-		factk = factk * i;
-	}
-
-	factnk = 1;
-	for (int i = 1; i <= n - k; ++i)
-	{
-		factnk = factnk * i;
-	}
-
-	cout << factn / (factk * factnk) << endl;
+	C = round(c);
+	cout << C << endl;
 
 	return EXIT_SUCCESS;
 }
