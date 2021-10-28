17d1d0e8486b4cf242006ef4d01dbf8a89bb2ab5
st080579 <st080579@student.spbu.ru>
2021-10-28 12:39:35 +0300

    2021-10-28

--- 2021-10-04-Homework3/2021-10-04-04/Source.cpp
+++ 2021-10-04-Homework3/2021-10-04-04/Source.cpp
@@ -7,33 +7,24 @@ int main(int argc, char* argv[])
 {
 	int n = 0;
 	int k = 0;
-	int f_n = 0;
-	int f_k = 0;
-	int f_n_k = 0;
+	double c = 0;
+	int C = 0;
 
 	cout << "Enter data:" << endl;
 	cin >> n >> k;
 	cout << "Exit data:" << endl;
 
-	f_n = 1;
-	for (int i = 1; i <= n; ++i)
-	{
-		f_n *= i;
-	}
+	c = 1;
+	C = 1;
 
-	f_k = 1;
-	for (int i = 1; i <= k; ++i)
-	{
-		f_k *= i;
-	}
-
-	f_n_k = 1;
 	for (int i = 1; i <= n - k; ++i)
 	{
-		f_n_k *= i;
+		double j = i;
+		c *= (k + j) / j;
 	}
 
-	cout << f_n / (f_k * f_n_k) << endl;
+	C = round(c);
+	cout << C << endl;
 
 	return EXIT_SUCCESS;
 }
